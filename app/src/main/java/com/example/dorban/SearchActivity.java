acb7707696044af30663237a32518a4b6cfac133
yuvalotm <yuvalotm@gmail.com>
2019-12-25 16:04:12 +0200

    loginactivity

---
+++ app/src/main/java/com/example/dorban/SearchActivity.java
@@ -0,0 +1,21 @@
+package com.example.dorban;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+        }
+    }
+}
--- app/src/main/java/com/example/dorban/TabsActivity.java
+++ app/src/main/java/com/example/dorban/TabsActivity.java
@@ -1,5 +1,7 @@
 package com.example.dorban;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,6 +18,7 @@ public class TabsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
